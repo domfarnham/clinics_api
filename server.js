38a77e484c7c89ac1d59a62102a3f68c04859fe8
domfarnham <dom@jscodebook.com>
2018-03-13 06:03:49 +0000

    Initital Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const clinics = require('./lib/clinics')
+
+
+// Routes
+app.use('/public', express.static(process.cwd() + '/public'))
+
+app.get('/clinics/postcode/:postcode', clinics.find)
+
+// Respond not found to all the wrong routes
+app.use(function (req, res, next) {
+  res.status(404)
+  res.type('txt').send('Not found')
+})
+
+// Error Middleware
+app.use(function (err, req, res, next) {
+  if (err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR')
+  }
+})
+
+const server = app.listen(3000, function () {
+  console.log('Node.js listening on port ' + 3000)
+})
+
+module.exports = server
